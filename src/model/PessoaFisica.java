20ac272192484e1c75cc03fe8c9073160082a2b4
Mayara Theodoro <47544451+mayarapthd@users.noreply.github.com>
2019-05-02 22:25:06 -0300

    Add files via upload

---
+++ src/model/PessoaFisica.java
@@ -0,0 +1,14 @@
+package model;
+
+public class PessoaFisica extends Pessoas {
+	
+	private String nomecompleto;
+	private long rg;
+	private long cpf;
+	private String datadenascimento;
+	
+	
+
+
+
+}
---
+++ src/view/Execucao.java
@@ -0,0 +1,15 @@
+package view;
+
+import model.PessoaFisica;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PessoaFisica pf = new PessoaFisica();
+		
+		pf.setcpf(49352575806);
+		pf.setendereco("R Renato Katsuya Sato");
+	}
+
+}
